ebbbb2fc21d5471b045dd390b4f6d2f634d554b0
EE8005 <nicolas.bolivar@externos-cl.cencosud.com>
2021-09-09 18:39:15 -0400

    adding validation to request

--- src/modules/courses/infrastructure/requests/course.request.ts
+++ src/modules/courses/infrastructure/requests/course.request.ts
@@ -1,15 +1,27 @@
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CourseRequest {
+  @IsString()
+  @IsNotEmpty()
   title: string;
 
+  @IsString()
+  @IsNotEmpty()
   category: string;
 
+  @IsString()
+  @IsNotEmpty()
   description: string;
 
+  @IsString()
+  @IsNotEmpty()
   image: string;
 
+  @IsString()
+  @IsNotEmpty()
   link: string;
 
+  @IsString()
+  @IsNotEmpty()
   teachers: string;
 }
