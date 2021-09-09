d9f8aabd2674bc65c47b4844dc7daac51faaa948
EE8005 <nicolas.bolivar@externos-cl.cencosud.com>
2021-09-08 22:55:03 -0400

    delete default app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CoursesModule } from './modules/courses/infrastructure/courses.module';
 import { CourseDataMapper } from './modules/courses/infrastructure/persistent/data-mapper/course.data.mapper';
 
@@ -18,7 +16,7 @@ import { CourseDataMapper } from './modules/courses/infrastructure/persistent/da
     }),
     CoursesModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
